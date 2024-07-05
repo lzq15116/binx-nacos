0037e38e9d80d5b14251f10c37b834f7c7b44e52
lzq <null@null>
2024-07-05 10:56:49 +0800

    加入gateway成功！

---
+++ binx-gateway/src/main/java/com/xisthebest/binxgateway/BinxGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xisthebest.binxgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinxGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinxGatewayApplication.class, args);
+    }
+
+}
---
+++ binx-gateway/src/test/java/com/xisthebest/binxgateway/BinxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xisthebest.binxgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinxGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
